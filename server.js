b4b2ecd0f7a257c7a6a0bc8611c379f5c5f9a6e2
Jasmin Bouasavatdy <jasminbouasavatdy@hotmail.com>
2022-03-25 14:39:30 -0400

    able to do  GET

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const { application } = require('express');
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +12,18 @@ app.use(express.urlencoded({
 }));
 
 
+app.get('/notes',(req, res) =>{
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+app.get('/api/notes', (req, res)=>{
+  res.json(`${req.method} request recieved to get notes`);
+
+  console.info(`${req.method} request received to get notes`);
+})
+
+app.post('/api/notes', (req, res)=>{
+  console.info(`${req.method} request recieved to add notes`)
+  res.json(`${req.method} request recieved to get notes`);
 
 
 app.get('*',(req, res)=>{
